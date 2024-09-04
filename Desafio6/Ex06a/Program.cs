12a40c191a82a1f392101ef7588237d972c46fce
Leonardo Heredia <84853719+LeoHeredia2004@users.noreply.github.com>
2024-09-04 09:06:32 -0300

    Add files via upload

---
+++ Desafio6/Ex06a/Program.cs
@@ -0,0 +1,31 @@
+﻿int tamanho = 100;
+int[] vetor = new int[tamanho];
+
+Random random = new Random();
+
+for(int i = 0;i < vetor.Length; i++){
+    vetor[i] = random.Next(100);
+}
+
+for(int i = 0;i < vetor.Length; i++){
+    Console.Write(vetor[i] + " ");
+}
+
+for(int i = 0;i < vetor.Length - 1;i++){
+    if(vetor[i] > vetor[i + 1]){
+        
+        int aux = vetor[i];
+        vetor[i] = vetor[i + 1];
+        vetor[i + 1] = aux;
+    }
+}
+
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+
+
+for(int i = 0;i < vetor.Length; i++){
+    Console.Write(vetor[i] + " ");
+}
+
+
