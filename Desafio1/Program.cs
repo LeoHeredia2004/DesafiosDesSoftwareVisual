ce698bcaf0392eae6eca4264cc95c2d88b6fca64
Leonardo Heredia <84853719+LeoHeredia2004@users.noreply.github.com>
2024-09-04 09:03:48 -0300

    Add files via upload

---
+++ Desafio1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Altura: ");
+        float altura = float.Parse(Console.ReadLine());
+
+        Console.Write("Base: ");
+        float baseT = float.Parse(Console.ReadLine());
+
+        float area = baseT * altura / 2;
+        Console.WriteLine("Área do triângulo: " + area);
+    }
+}
+
